f1fed1b9a2099d06762eb4283c0bd0b37831eb8b
Bruno Eduardo <39205995+BrunoEduardo1@users.noreply.github.com>
2023-09-03 18:33:28 -0300

    tests: test cases for account service and create mock repository

---
+++ tests/Mocks/AccountRepositoryMock.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Mocks;
+
+use App\Contracts\Repositories\AccountRepositoryInterface;
+use App\Models\Account;
+
+class AccountRepositoryMock implements AccountRepositoryInterface
+{
+    protected $model = Account::class;
+    protected $perPage = 15;
+
+    public function __construct()
+    {
+        $this->initializeData();
+    }
+
+    private function initializeData()
+    {
+        $this->data = [
+            ['id' => 1, 'balance' => 100],
+            ['id' => 2, 'balance' => 120],
+        ];
+    }
+
+    public function getAll()
+    {
+        return array_map(function ($item) {
+            return new $this->model($item);
+        }, $this->data);
+    }
+
+    public function getById($accountId)
+    {
+        foreach ($this->data as $item) {
+            if ($item['id'] == $accountId) {
+                return new $this->model($item);
+            }
+        }
+
+        return null;
+    }
+
+    public function create($data)
+    {
+        $this->data[] = $data;
+
+        return new $this->model($data);
+    }
+
+    public function updateBalance($accountId, $amount)
+    {
+        foreach ($this->data as $account) {
+            if ($account['id'] === $accountId) {
+                $account['balance'] -= $amount;
+                return new $this->model($account);
+            }
+        }
+        return null;
+    }
+
+    public function getBalance($accountId)
+    {
+        $account = $this->getById($accountId);
+
+        return $account->balance;
+    }
+}
